445879c3778c100d97a3cf6590aa4bb89226aaf0
sunianOvO <89292615+sunianOvO@users.noreply.github.com>
2022-09-10 23:08:18 +0800

    Add files via upload

---
+++ cloud-shell/fuckb1s.py
@@ -0,0 +1,39 @@
+import io
+import json
+import time
+import random
+from contextlib import redirect_stdout
+from azure.cli.core import get_default_cli
+
+locations = ['eastus', 'eastus2', 'southcentralus', 'westus3', 'australiaeast',
+             'southeastasia', 'northeurope', 'swedencentral', 'uksouth', 'westeurope',
+             'centralus', 'southafricanorth', 'centralindia', 'eastasia', 'japaneast',
+             'koreacentral', 'canadacentral', 'francecentral', 'norwayeast', 'switzerlandnorth',
+             'uaenorth', 'brazilsouth', 'qatarcentral', 'northcentralus', 'westus', 'australiacentral',
+             'australiasoutheast', 'japanwest', 'southindia', 'canadaeast', 'ukwest']
+
+vm_size = "Standard_B1s"
+size_count = 5
+fuck = random.randint(0,9999999999)
+
+get_default_cli().invoke(['group', 'create', '--name', 'ResourceGroup',
+                          '--location', 'eastasia'])
+print("Resource Group Created Successfully")
+for location in locations: 
+    count = 0
+    for a in range(0, size_count):
+        count += 1
+        print(str(count) + " of " + str(size_count) + " " + str(vm_size) + " VM in " + str(location) + " is being Created")
+        get_default_cli().invoke(
+            ['vm', 'create', '--resource-group', 'ResourceGroup', '--name',
+             f'{location}-{vm_size}-{count}-{fuck}', '--image', 'UbuntuLTS',
+             '--size', f'{vm_size}', '--location', f'{location}', '--admin-username',
+             'axuan', '--admin-password', '320Hanyue1827...', '--custom-data',
+             './.XX/cloud-init.txt', "--no-wait"])
+
+print("\n")
+print("VM Created Successfully")
+print("\n")
+print("VM List: ")
+get_default_cli().invoke(['vm', 'list', '--query', '[*].name'])
+print("\n")
